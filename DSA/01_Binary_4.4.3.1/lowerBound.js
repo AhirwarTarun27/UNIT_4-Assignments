372803224389c11911e3a5735b0161537ab5a309
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2021-12-27 22:10:25 +0530

    updated

---
+++ DSA/01_Binary_4.4.3.1/lowerBound.js
@@ -0,0 +1,51 @@
+function runProgram(input) {
+  // Write code here
+  var input = input.trim().split("\n");
+  let [n, k] = input[0].trim().split(" ").map(Number);
+  let arr = input[1].trim().split(" ").map(Number);
+  function lowerBond(n, k, arr) {
+    let low = 0;
+    let high = n - 1;
+    let ans = -1;
+
+    while (high - low > 1) {
+      let mid = Math.floor(low + (high - low) / 2);
+      if (k <= arr[mid]) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    if (arr[low] == k) {
+      return low;
+    } else if (arr[high] == k) {
+      return high;
+    } else {
+      return -1;
+    }
+  }
+  console.log(lowerBond(n, k, arr));
+}
+
+if (process.env.USERNAME === "coder") {
+  runProgram(`5 3
+  1 1 2 2 5`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
